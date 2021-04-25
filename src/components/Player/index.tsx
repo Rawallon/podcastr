1d3ec99ddd0b196c9881697526af888a11b10866
rawallon <rawallon@gmail.com>
2021-04-25 14:14:35 -0300

    Evitar scroll ao pressionar espaço

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -41,6 +41,7 @@ export default function Player() {
         setAudioVolume(Number(storageVolume));
       }
       document.addEventListener('keydown', (e) => {
+        e.preventDefault();
         if (e.code === 'Space') {
           if (audioRef.current.paused) {
             audioRef.current.play();
