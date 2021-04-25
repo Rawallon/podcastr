44d755bdf8310b7cc92d6202b77ea3a8b30db4e0
rawallon <rawallon@gmail.com>
2021-04-25 14:08:37 -0300

    PWA com next-pwa

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,7 +10,27 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap"
             rel="stylesheet"
           />
-          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#c3dca6" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           <Main />
